b2bba88725890f90a2515bf92d58e84916cba6b3
ZAIDAT Zoheir <z.zaidat.pro@gmail.com>
2026-09-24 19:54:57 +0100

    mern httpOnly v3

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ import todoRoutes from './routes/todosRoute.js';
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173', credentials: true                
+  origin: 'http://localhost:5173' || 'https://mern-todo-jwt-mongo-crud.onrender.com', credentials: true                
 }));
 
 app.use(cookieParser());
