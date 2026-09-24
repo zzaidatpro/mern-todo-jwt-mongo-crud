ff20d5192c63876e2365f58ee5f47866cad2f4f9
ZAIDAT Zoheir <z.zaidat.pro@gmail.com>
2026-09-24 21:02:25 +0100

    mern httpOnly v5

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api' || 'https://todo-jwt-mongo.vercel.app/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
   withCredentials: true, // Crucial pour l'envoi/réception automatique des cookies HttpOnly
 });
 
