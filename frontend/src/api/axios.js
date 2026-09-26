03db3a463e4b2968ced289db9ec0f2f9abb1c906
ZAIDAT Zoheir <z.zaidat.pro@gmail.com>
2026-09-26 11:35:27 +0100

    mern httpOnly v6

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.BACKEND_API_URL || 'http://localhost:5000/api',
+  baseURL: 'https://backend-mern-http-only.vercel.app',
   withCredentials: true, // Crucial pour l'envoi/réception automatique des cookies HttpOnly
 });
 
--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,23 +1,23 @@
-import api from '../api/axios';
+import API from '../api/axios';
 
 export const authService = {
   login: async (credentials) => {
-    const response = await api.post('/auth/login', credentials);
+    const response = await API.post('/auth/login', credentials);
     return response.data;
   },
 
   register: async (userData) => {
-    const response = await api.post('/auth/register', userData);
+    const response = await API.post('/auth/register', userData);
     return response.data;
   },
 
   logout: async () => {
-    const response = await api.post('/auth/logout');
+    const response = await API.post('/auth/logout');
     return response.data;
   },
 
   getMe: async () => {
-    const response = await api.get('/auth/me'); 
+    const response = await API.get('/auth/me'); 
     return response.data;
   },
 };
